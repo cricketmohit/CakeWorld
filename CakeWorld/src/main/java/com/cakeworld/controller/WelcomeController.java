f8e0bc5bc877f30c72b46969c4aca2b0b30f5c64
Mohit Sharma <cricketmohit@gmail.com>
2018-08-28 10:02:36 +0200

    HTML Template, JS and images

--- CakeWorld/src/main/java/com/cakeworld/controller/WelcomeController.java
+++ CakeWorld/src/main/java/com/cakeworld/controller/WelcomeController.java
@@ -22,6 +22,46 @@ public class WelcomeController {
 	
 	@RequestMapping("/")
 	String entry() {
+		return "index";
+	}
+	@RequestMapping("/index")
+	String index() {
+		return "index";
+	}
+	@RequestMapping("/404")
+	String noPage() {
+		return "404";
+	}
+	@RequestMapping("/about")
+	String about() {
+		return "about";
+	}
+	@RequestMapping("/blog")
+	String blog() {
+		return "blog";
+	}
+	@RequestMapping("/contact")
+	String contact() {
+		return "contact";
+	}
+	@RequestMapping("/gallery")
+	String gallery() {
+		return "gallery";
+	}
+	@RequestMapping("/home")
+	String home() {
 		return "home";
+	}
+	@RequestMapping("/login")
+	String login() {
+		return "login";
+	}
+	@RequestMapping("/register")
+	String register() {
+		return "register";
+	}
+	@RequestMapping("/single")
+	String single() {
+		return "single";
 	}
 }
