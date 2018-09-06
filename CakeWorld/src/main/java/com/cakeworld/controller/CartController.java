02e1e59629a065231706ebe42a1bef6571d21ad3
Mohit Sharma <cricketmohit@gmail.com>
2018-09-06 22:33:09 +0200

    added cart page

--- CakeWorld/src/main/java/com/cakeworld/controller/CartController.java
+++ CakeWorld/src/main/java/com/cakeworld/controller/CartController.java
@@ -1,11 +1,17 @@
 package com.cakeworld.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,4 +43,32 @@ public class CartController {
 		
 		return "index";
     }
+	
+	
+
+	@RequestMapping(value = "/cart", method = RequestMethod.GET)
+	public String checkOut(Model model,
+			@CookieValue(value = "cookiecartcounts", defaultValue = "0") String cookiecartcounts) {
+		Map<String, List<Menu>> menuFromDB = getMenuFromDB(cookiecartcounts.split("\\*"));
+		model.addAttribute("checkoutCart", menuFromDB);
+		return "cart";
+	}
+
+	public Map<String, List<Menu>> getMenuFromDB(String[] ids) {
+		List<Menu> innerList;
+		Map<String, List<Menu>> menuMap = new HashMap<String, List<Menu>>();
+		for (String id : ids) {
+			Menu menu = menuRepository.findOne(Integer.valueOf(id));
+			if (menu != null) {
+				if (menuMap.get(menu.getName()) != null) {
+					menuMap.get(menu.getName()).add(menu);
+				} else {
+					innerList = new ArrayList<Menu>();
+					innerList.add(menu);
+					menuMap.put(menu.getName(), innerList);
+				}
+			}
+		}
+		return menuMap;
+	}
 }
