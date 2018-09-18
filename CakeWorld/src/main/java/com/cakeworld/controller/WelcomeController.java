9fddeb9727ee0dc524c87c069c01ae7265cfc3c3
Mohit Sharma <cricketmohit@gmail.com>
2018-09-19 01:51:13 +0200

    One Last commit from Mac

--- CakeWorld/src/main/java/com/cakeworld/controller/WelcomeController.java
+++ CakeWorld/src/main/java/com/cakeworld/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package com.cakeworld.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -14,12 +15,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.cakeworld.main.MenuRepository;
 import com.cakeworld.main.UserRepository;
 import com.cakeworld.model.Menu;
+import com.cakeworld.model.Pincode;
 import com.cakeworld.model.User;
 
 @Controller
@@ -118,6 +123,69 @@ public class WelcomeController {
 		model.addAttribute("galleryMenu", galleryMenu);
 
 		return "index";
+	}
+	
+	@RequestMapping("/product")
+	String product(Model model, HttpSession session, HttpServletResponse response,
+			@CookieValue(value = "userEmailCookie", defaultValue = "") String userEmailCookie,
+			@CookieValue(value = "cookiecartcounts", defaultValue = "0") String cookiecartcounts) {
+		if (!userEmailCookie.equalsIgnoreCase("")) {
+			User user = userRepository.findByEmail(userEmailCookie).get(0);
+			model.addAttribute("loggedInUser", user.getName());
+		}
+		Iterable<Menu> findAll = menuRepository.findAll();
+		List<Menu> cakeMenu = new ArrayList<Menu>();
+		List<Menu> ccMenu = new ArrayList<Menu>();
+		List<Menu> savMenu = new ArrayList<Menu>();
+		List<Menu> biscuitMenu = new ArrayList<Menu>();
+		List<Menu> galleryMenu = new ArrayList<Menu>();
+		for (Menu menu : findAll) {
+			switch (menu.getCategory().getId()) {
+			case 1:
+				cakeMenu.add(menu);
+				break;
+			case 2:
+				ccMenu.add(menu);
+				break;
+			case 3:
+				savMenu.add(menu);
+				break;
+			case 4:
+				biscuitMenu.add(menu);
+				break;
+			default:
+				break;
+			}
+			if (galleryMenu.size() < 12) {
+				galleryMenu.add(menu);
+			}
+		}
+		model.addAttribute("cakeMenu", cakeMenu);
+		model.addAttribute("ccMenu", ccMenu);
+		model.addAttribute("savMenu", savMenu);
+		model.addAttribute("biscuitMenu", biscuitMenu);
+		model.addAttribute("galleryMenu", galleryMenu);
+
+		return "product";
+	}
+	
+	@RequestMapping(value = "/product", method = RequestMethod.GET)
+	public  String checkPinCode(@ModelAttribute("productId") String productId, Model model, 
+			@CookieValue(value = "cookiecartcounts", defaultValue = "0") String cookiecartcounts,
+			@CookieValue(value = "userEmailCookie", defaultValue = "") String userEmailCookie) {
+		
+		Menu product = menuRepository.findOne(Integer.valueOf(1));
+		
+		
+		model.addAttribute("product", product);
+		if (!userEmailCookie.equalsIgnoreCase("")) {
+			User user = userRepository.findByEmail(userEmailCookie).get(0);
+			model.addAttribute("loggedInUser", user.getName());
+			model.addAttribute("discountEligible", "discountEligible");
+		}
+		
+		
+		return "product";
 	}
 
 	@ExceptionHandler
--- CakeWorld/src/main/resources/static/js/localList.js
+++ CakeWorld/src/main/resources/static/js/localList.js
@@ -75,9 +75,92 @@ function removeItem(id)
 	
 }
 
-function payment()
-{
-	
-	
 
+function imageZoom(imgID, resultID) {
+  var img, lens, result, cx, cy;
+  img = document.getElementById(imgID);
+  
+  result = document.getElementById(resultID);
+  /*create lens:*/
+  lens = document.createElement("DIV");
+  lens.setAttribute("class", "img-zoom-lens");
+  lens.setAttribute("style", "display:none");
+  /*insert lens:*/
+  img.parentElement.insertBefore(lens, img);
+  /*calculate the ratio between result DIV and lens:*/
+  cx = result.offsetWidth / lens.offsetWidth;
+  cy = result.offsetHeight / lens.offsetHeight;
+  /*set background properties for the result DIV:*/
+  result.style.backgroundImage = "url('" + img.src + "')";
+  result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+  /*execute a function when someone moves the cursor over the image, or the lens:*/
+  lens.addEventListener("mousemove", moveLens);
+  img.addEventListener("mousemove", moveLens);
+  /*and also for touch screens:*/
+  lens.addEventListener("touchmove", moveLens);
+  img.addEventListener("touchmove", moveLens);
+  function moveLens(e) {
+	  lens.setAttribute("style", "display:block");
+    var pos, x, y,p,q;
+    /*prevent any other actions that may occur when moving over the image:*/
+    e.preventDefault();
+    /*get the cursor's x and y positions:*/
+    pos = getCursorPos(e);
+    
+    /*calculate the position of the lens:*/
+    x = pos.x - (lens.offsetWidth / 2);
+    y = pos.y - (lens.offsetHeight / 2);
+    /*prevent the lens from being positioned outside the image:*/
+    p=x;
+    q=y;
+    if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;} 
+    if (x < 0) {x = 0;}
+    if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+    if (y < 0) {y = 0;}
+
+    if(p - (lens.offsetWidth / 2) + 10 > img.width - lens.offsetWidth){
+    	 lens.setAttribute("style", "display:none");
+    }
+   
+    if(q - (lens.offsetHeight / 2) + 10 > img.height - lens.offsetWidth){
+   	 lens.setAttribute("style", "display:none");
+    }
+   
+ 	if(q + (lens.offsetHeight / 2) - 10 < 0){
+     	 lens.setAttribute("style", "display:none");
+   	
+   }
+    /*set the position of the lens:*/
+    lens.style.left = x + "px";
+    lens.style.top = y + "px";
+    
+    /*display what the lens "sees":*/
+    result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+    
+  }
+  function getCursorPos(e) {
+	    var a, x = 0, y = 0;
+	    e = e || window.event;
+	    /*get the x and y positions of the image:*/
+	    a = img.getBoundingClientRect();
+	    /*calculate the cursor's x and y coordinates, relative to the image:*/
+	    
+	    x = e.pageX - a.left;
+	    y = e.pageY - a.top;
+	  
+	    /*consider any page scrolling:*/
+	    x = x - window.pageXOffset;
+	    y = y - window.pageYOffset;
+	    return {x : x, y : y};
+	  }
+ 
+  function getMouseCoords(event) {
+      var x = event.clientX;
+      var y = event.clientY;
+      var img = document.getElementById(imgID);
+      alert(img.width);
+     
+  }
+  
 }
+
